5d0ab50ea56e74926a1f170aa37283775ee3071b
Jenna Scholl <bin19005@byui.edu>
2023-05-04 14:55:47 -0600

    king unit tests outlined

--- king.h
+++ king.h
@@ -1,9 +1,26 @@
+/***********************************************************************
+ * Header File:
+ *    King
+ * Author:
+ *    Jenna Scholl
+ * Summary:
+ *    All the unique functionality of the king
+ ************************************************************************/
+
 #pragma once
 
 #include "piece.h"
 
+class TestKing;
+
+/*********************************************
+ * KING
+ * The most important piece in the game
+ *********************************************/
 class King : public Piece
 {
+   friend TestKing;
+
 public:
    char getLetter();
    void display(ogstream gout);
--- testKing.h
+++ testKing.h
@@ -1,1 +1,216 @@
+/***********************************************************************
+ * Header File:
+ *    Test King : Test the King class
+ * Author:
+ *    Jenna Scholl
+ * Summary:
+ *    All the unit tests for King
+ ************************************************************************/
+
 #pragma once
+
+#include "king.h"
+
+/********************************
+* TEST KING
+* A friend class for King which contains all its unit tests
+********************************/
+class TestKing
+{
+public:
+   void run()
+   {
+      test_getMoves_free();
+      test_getMoves_blocked();
+      test_getMoves_capture();
+      test_getMoves_castle();
+      test_getMoves_castle_king_moved();
+      test_getMoves_castle_rook_moved();
+      test_getMoves_castle_blocked();
+      test_getMoves_castle();
+   }
+
+private:
+   /********************************
+   * GET MOVES TEST - white king free
+   * +---a-b-c-d-e-f-g-h---+
+   * |                     |
+   * 8                     8
+   * 7                     7
+   * 6         . . .       6
+   * 5         . k .       5
+   * 4         . . .       4
+   * 3                     3
+   * 2                     2
+   * 1                     1
+   * |                     |
+   * +---a-b-c-d-e-f-g-h---+
+   ********************************/
+   void test_getMoves_free()
+   {
+      // setup
+
+      // exercise
+
+      // verify
+
+      // teardown
+   }
+
+   /********************************
+   * GET MOVES TEST - white king blocked
+   * +---a-b-c-d-e-f-g-h---+
+   * |                     |
+   * 8                     8
+   * 7                     7
+   * 6         p p p       6
+   * 5         p k p       5
+   * 4         p p p       4
+   * 3                     3
+   * 2                     2
+   * 1                     1
+   * |                     |
+   * +---a-b-c-d-e-f-g-h---+
+   ********************************/
+   void test_getMoves_blocked()
+   {
+      // setup
+
+      // exercise
+
+      // verify
+
+      // teardown
+   }
+
+   /********************************
+   * GET MOVES TEST - white king capture
+   * +---a-b-c-d-e-f-g-h---+
+   * |                     |
+   * 8                     8
+   * 7                     7
+   * 6         P P P       6
+   * 5         P k P       5
+   * 4         P P P       4
+   * 3                     3
+   * 2                     2
+   * 1                     1
+   * |                     |
+   * +---a-b-c-d-e-f-g-h---+
+   ********************************/
+   void test_getMoves_capture()
+   {
+      // setup
+
+      // exercise
+
+      // verify
+
+      // teardown
+   }
+
+   /********************************
+   * GET MOVES TEST - white king castle (king moved)
+   * +---a-b-c-d-e-f-g-h---+
+   * |                     |
+   * 8                     8
+   * 7                     7
+   * 6                     6
+   * 5                     5
+   * 4                     4
+   * 3                     3
+   * 2         p p p       2
+   * 1   r     . k .   r   1
+   * |                     |
+   * +---a-b-c-d-e-f-g-h---+
+   ********************************/
+   void test_getMoves_castle_king_moved()
+   {
+      // setup
+
+      // exercise
+
+      // verify
+
+      // teardown
+   }
+
+   /********************************
+   * GET MOVES TEST - white king castle (rook moved)
+   * +---a-b-c-d-e-f-g-h---+
+   * |                     |
+   * 8                     8
+   * 7                     7
+   * 6                     6
+   * 5                     5
+   * 4                     4
+   * 3                     3
+   * 2         p p p       2
+   * 1   r     . k .   r   1
+   * |                     |
+   * +---a-b-c-d-e-f-g-h---+
+   ********************************/
+   void test_getMoves_castle_rook_moved()
+   {
+      // setup
+
+      // exercise
+
+      // verify
+
+      // teardown
+   }
+
+   /********************************
+   * GET MOVES TEST - white king castle (blocked)
+   * +---a-b-c-d-e-f-g-h---+
+   * |                     |
+   * 8                     8
+   * 7                     7
+   * 6                     6
+   * 5                     5
+   * 4                     4
+   * 3                     3
+   * 2         p p p       2
+   * 1   r p   . k . p r   1
+   * |                     |
+   * +---a-b-c-d-e-f-g-h---+
+   ********************************/
+   void test_getMoves_castle_blocked()
+   {
+      // setup
+
+      // exercise
+
+      // verify
+
+      // teardown
+   }
+
+   /********************************
+   * GET MOVES TEST - white king castle
+   * +---a-b-c-d-e-f-g-h---+
+   * |                     |
+   * 8                     8
+   * 7                     7
+   * 6                     6
+   * 5                     5
+   * 4                     4
+   * 3                     3
+   * 2         p p p       2
+   * 1   r   . . k . . r   1
+   * |                     |
+   * +---a-b-c-d-e-f-g-h---+
+   ********************************/
+   void test_getMoves_castle()
+   {
+      // setup
+
+      // exercise
+
+      // verify
+
+      // teardown
+   }
+
+};
