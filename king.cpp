c7237439b2490e5650ca51fe3853b4d59262d0fc
Ashley Offret <ashleyoffret1@gmail.com>
2023-05-10 15:50:49 -0600

    Added comments to king.cpp
    I added comments in king.cpp to try and better understand the code.

--- king.cpp
+++ king.cpp
@@ -20,6 +20,8 @@ set<Move> King::getMoves(Board* board, Move move)
 {
    set<Move> moves;
 
+   //Meant to calculate each potential location 
+   // immedately around the king. 
    Position delta[8] =
    {
       {-1,  1}, {0,  1}, {1,  1},
@@ -27,19 +29,24 @@ set<Move> King::getMoves(Board* board, Move move)
       {-1, -1}, {0, -1}, {1, -1}
    };
 
+   //Make array of moves immediately around king. 
    //moves = getMovesNoSlide(board, delta);
 
+   //If the king hasn't moved, and the rook has
+   //2 open spaces and hasn't moved, then you can castle.
    if (!isMoved())
    {
       Position posSpace(position.getRow(), 5);
       Position posMove(position.getRow(), 6);
       Position posRook(position.getRow(), 7);
 
+
       if ((*board)[posMove]->getType() == SPACE
          && (*board)[posSpace]->getType() == SPACE
          && (*board)[posRook]->getType() == ROOK
          && (*board)[posRook]->isMoved() == false)
       {
+        //Move the king to the new castling position. 
          Move move;
          move.setSrc(getPosition());
          move.setDest(posMove);
@@ -48,10 +55,15 @@ set<Move> King::getMoves(Board* board, Move move)
       }
    }
 
+   //For each move in delta, 
    for (int i = 0; i < 8; i++)
    {
+      //Add appropriate modifiers to each location. 
       r = row + moves[i].row;
       c = col + moves[i].col;
+
+      //Only allow if my same color isn't in that spot. 
+      //Insert numerical location (0-64) as possible. 
       if (amBlack && isNotBlack(board, r, c))
          possible.insert(r * 8 + c);
       if (!amBlack && isNotWhite(board, r, c))
